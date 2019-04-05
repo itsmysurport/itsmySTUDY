eda6501a7a6c8878827b980d68158d818dbea066
SeungHo Choi <36769771+itsmysurport@users.noreply.github.com>
2019-04-05 11:10:11 +0900

    8LED

---
+++ School/Embedded/Microprocessor/LED_8_Toggle_Switch_2/LED_8_Toggle_Switch_2/main.c
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	/* Replace with your application code */
+	DDRC = 0x00;
+	DDRD= 0xff;
+	PORTD = 0x00;
+	while (1)
+	{
+		if(!(PINC & 0x01))
+		{
+			PORTD = ~(PORTD);
+			_delay_ms(300);
+		}
+	}
+}
