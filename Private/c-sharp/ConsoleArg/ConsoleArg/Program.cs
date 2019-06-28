10aa970a47dc7fc9fb7227f5040516704022c2b9
itsmysurport <itsmysurport@gmail.com>
2019-06-28 11:20:06 +0900

    C# Study started

---
+++ Private/c-sharp/ConsoleArg/ConsoleArg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleArg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usgae : Helloworld.exe <NAME>.");
+                return;
+            }
+            Console.WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
